c3eaaf0736ab911ced5846ef95a46ef482382e1d
Sergio Alcantara <iserg9@gmail.com>
2012-08-19 14:45:34 -0700

    First pages commit

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DynDB"
+    ],
+    "modules": [
+        "dyndb"
+    ],
+    "allModules": [
+        {
+            "displayName": "dyndb",
+            "name": "dyndb",
+            "description": "DynDB 0.0.3 - (c) 2012 Sergio Alcantara\nProvides the base DynDB constructor"
+        }
+    ]
+} };
+});
